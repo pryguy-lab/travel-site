3052772d8d7f428df5a1defd79de398c4ebdf132
Mike Pry <mikepry@hotmail.com>
2023-01-15 17:46:31 -0500

    setting up hot reload on webpack. Changed contentBase to static in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,12 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"),
   },
+  devServer: {
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
